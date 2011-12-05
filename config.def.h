2fc94f4d8c0cb48767611b03a7ba502dc368dc95
Suraj N. Kurapati <sunaku@gmail.com>
2011-12-05 11:10:32 -0800

    bottom_gap patch to make room for dzen2 statusbar

--- config.def.h
+++ config.def.h
@@ -12,6 +12,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
+static const unsigned int bottomgap = 16;
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
