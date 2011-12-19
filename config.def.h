c0ba713fe7a324a2c23d1778f5970a1dcac08b52
Suraj N. Kurapati <sunaku@gmail.com>
2011-12-19 11:19:48 -0800

    revert config.def.h to make merging easier

--- config.def.h
+++ config.def.h
@@ -27,13 +27,11 @@ static const float mfact      = 0.55; /* factor of master area size [0.05..0.95]
 static const int nmaster      = 1;    /* number of clients in master area */
 static const Bool resizehints = True; /* True means respect size hints in tiled resizals */
 
-#include "grid.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "HHH",      grid },
 };
 
 /* key definitions */
